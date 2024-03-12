74f48112091e8d667310cdd738e5ceeba505ba74
Quangdao Nguyen <qnguyen@nvisia.com>
2024-03-12 14:22:31 -0500

    Forgot about json

--- src/routes/apps.json/+server.ts
+++ src/routes/apps.json/+server.ts
@@ -1,12 +1,11 @@
 import YAML from 'yaml';
 import fs from 'fs';
+import { json } from '@sveltejs/kit';
 
 export function GET() {
 	var yaml = fs.readFileSync('./data/apps.yaml', 'utf-8');
 	var apps = YAML.parse(yaml);
-	return new Response(JSON.stringify(apps), {
-		headers: { 'Content-Type': 'application/json' }
-	});
+	return json(apps);
 }
 
 export const prerender = true;
